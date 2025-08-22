c5a1846a24dc5c454a4c45af126a9b2108148a82
vali-tio <brovalitio@gmail.com>
2025-08-23 01:56:39 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("pwa-cache").then(cache => {
+    caches.open("jadwal-cache").then(cache => {
       return cache.addAll([
         "index.html",
         "offline.html"
